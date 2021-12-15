019fd95b5ba851e4302bac91eafa1c4ccd73a2bd
Andy Bonnetto <andy.bonnetto@epfl.ch>
2021-12-15 18:49:22 +0100

    full launcher for healthy and deg

--- Spatio_temporal_modeling/launcher.m
+++ Spatio_temporal_modeling/launcher.m
@@ -25,16 +25,80 @@ p_spacing = 0;
 pp = [p_duration;p_spacing];
 L = light_to_cells(mat3D_full,n_CR,modes,pp,0);
 disp("Initial values and initial matrice generation...")
-M_cells_ref2 = Temporal_modeling_matrix(cell_list,{[],[]},L,Ma);
+M_cells_ref = Temporal_modeling_matrix(cell_list,{[],[]},L,Ma);
 %%
 %------- Start from here if you wanna test new electrical pulses ----
 
 % Get initial voltages and matrices for non degenerated cells
-V_m_init = M_cells_ref2.V_S(:,end);
+V_m_init = M_cells_ref.V_S(:,end);
 % n1 = length(cell_list_full);
 % n2 = length(cell_list);
 % V_m_init = V_m_init(n1-n2+1:n1,:);
 % V_m_init = V_m_init(fp_indices,:);
+M_init = M(cell_list);
+
+if Light_or_Electrode 
+    disp("Declare light protocol...")
+    n_tot = size(cell_list,2);
+    modes = ["disk-plus-100","disk-plus-40","disk-minus-40", "disk-minus-100"];
+    p_duration = 29e-3;
+    p_spacing = 157e-3;
+    pp = zeros(2,size(modes,2));
+    for i = 1:size(modes,2)
+        pp(:,i)= [(i-1)*p_spacing;p_duration];
+    end
+    L = light_to_cells(mat3D,n_CR,modes,pp,1);
+    disp("Pulsed light Stimulation")
+    Delta_Ve = zeros(M_cells.N_cells,Constants.t_size);
+    M_cells_h = Temporal_modeling_matrix(cell_list_d,{M_init,V_m_init},L,Ma, Delta_Ve);
+else
+    disp("Electrical stimulation")
+    vis_pulse = 0;
+    L = [];
+    Delta_Ve = generate_electrical_pulse(dVe.Delta_Ve,cell_list_d,vis_pulse,1:length(cell_list));
+    M_cells_h = Temporal_modeling_matrix(cell_list,{M_init,V_m_init},L,Ma,Delta_Ve);
+end
+
+% Visualization and Save plots
+disp("VM plots")
+figure()
+name_list = ["CR","HRZ","BP_on","BP_off","AM_WF_on","AM_WF_off","AM_NF_on","GL_on","GL_off"];
+names = name_list(logical(Pop_represented));
+
+k=1;
+for name = names
+    subplot(3,3,k)
+    d = M_cells_h.names == name;
+    plot(linspace(0,Constants.simulation_duration,Constants.t_size+1), M_cells_h.V_m(d,:)*1e3)
+    title(name)
+    xlabel("Time [s]")
+    ylabel("Voltage [mV]")
+    k = k +1;
+end
+saveas(gcf, strcat(folder_path,"V_m subplots"))
+
+if Light_or_Electrode
+    norm_V = 9e-3;
+else
+    norm_V = 10e-3;
+end
+% Color plots
+plot_membrane_voltage(M_cells_h,mat3D,Pop_represented,norm_V,folder_path)
+
+% Calculate Firing rate
+new_thresholds = [-62e-3,-53.2e-3];
+S = get_firing_rate(M_cells_h,1,folder_path, new_thresholds,[]);
+
+%%
+
+%------- Start from here if you wanna test new electrical pulses ----
+
+% Get initial voltages and matrices for non degenerated cells
+V_m_init = M_cells_ref.V_S(:,end);
+% n1 = length(cell_list_full);
+% n2 = length(cell_list);
+% V_m_init = V_m_init(n1-n2+1:n1,:);
+V_m_init = V_m_init(fp_indices,:);
 M_init = M(cell_list);
 
 if Light_or_Electrode 
@@ -55,8 +119,8 @@ else
     disp("Electrical stimulation")
     vis_pulse = 0;
     L = [];
-    Delta_Ve = generate_electrical_pulse(dVe.Delta_Ve,cell_list_d,vis_pulse,1:length(cell_list));
-    M_cells_h = Temporal_modeling_matrix(cell_list,{M_init,V_m_init},L,Ma,Delta_Ve);
+    Delta_Ve = generate_electrical_pulse(dVe.Delta_Ve,cell_list_d,vis_pulse,fp_indices);
+    M_cells = Temporal_modeling_matrix(cell_list_d,{M_init,V_m_init},L,Ma,Delta_Ve);
 end
 
 % Visualization and Save plots
@@ -83,12 +147,11 @@ else
     norm_V = 10e-3;
 end
 % Color plots
-plot_membrane_voltage(M_cells,mat3D,Pop_represented,norm_V,folder_path)
+plot_membrane_voltage(M_cells,mat3D(fp_indices,:),Pop_represented,norm_V,folder_path)
 
 % Calculate Firing rate
 new_thresholds = [-62e-3,-53.2e-3];
 S = get_firing_rate(M_cells,1,folder_path, new_thresholds,[]);
-
 
 %% Section to replot after simulation
 
@@ -96,10 +159,10 @@ k=1;
 figure()
 for name = names
     subplot(3,3,k)
-    d = M_cells_h.names == name;
-    d2 = vecnorm([mat3D(:,1),mat3D(:,2)].') < 50e-6;
+    d = M_cells.names == name;
+    d2 = vecnorm([mat3D(:,1),mat3D(:,2)].') < 300e-6;
     d3 = d&d2;
-    plot(linspace(0,Constants.simulation_duration,Constants.t_size+1), M_cells_h.V_m(d3,:)*1e3)
+    plot(linspace(0,Constants.simulation_duration,Constants.t_size+1), M_cells.V_m(d3,:)*1e3)
     title(name)
     xlabel("Time [s]")
     ylabel("Voltage [mV]")
